5ef519d5ced476d3b0eb694e37dcf4dff8f17459
Vivek_V_Vijayan <71512595+Vivekvvijayan@users.noreply.github.com>
2023-09-01 17:28:13 +0530

    dd

--- app/src/main/res/layout/Constants/Constants.kt
+++
@@ -1,7 +0,0 @@
-package com.example.carapp.Constants
-
-object Constants {
-
-    var BASE_URL_QUOTES = "https://ron-swanson-quotes.herokuapp.com/"
-    var BASE_URL_CARS = "https://vpic.nhtsa.dot.gov/"
-}
